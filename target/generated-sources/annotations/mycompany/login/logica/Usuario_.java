64073926c646f34ca94eadaf9586580fa7ef00e0
Monica Quiroz <monica_qs98@outlook.com>
2026-03-27 23:24:57 -0600

    Primer commit

---
+++ target/generated-sources/annotations/mycompany/login/logica/Usuario_.java
@@ -0,0 +1,15 @@
+package mycompany.login.logica;
+
+import javax.annotation.processing.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2026-03-27T21:45:31", comments="EclipseLink-2.7.12.v20230209-rNA")
+@StaticMetamodel(Usuario.class)
+public class Usuario_ { 
+
+    public static volatile SingularAttribute<Usuario, String> contrasenia;
+    public static volatile SingularAttribute<Usuario, Integer> id;
+    public static volatile SingularAttribute<Usuario, String> nombreUsuario;
+
+}
